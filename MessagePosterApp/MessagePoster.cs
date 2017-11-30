cbee982e020a6e6521472c6bfa5a366fb1df9065
Michael Okarimia <git@michaelokarimia.com>
2017-11-30 20:04:54 +0000

    Adds first failing test

---
+++ MessagePosterApp/MessagePoster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MessagePosterApp
+{
+    public class MessagePoster
+    {
+        public MessagePoster()
+        {
+        }
+
+        public string GetUserMessages(string name)
+        {
+            return "";
+        }
+    }
+}
---
+++ Tests/MessagePosterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MessagePosterApp;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MessagePosterTests
+    {
+        [Test]
+        public void PrintsUserNotFoundForUnknownUsernameEntered()
+        {
+            MessagePoster subject = new MessagePoster();
+            Assert.AreEqual("User name not found", subject.GetUserMessages("joe bloggs"));
+        }
+    }
+}
