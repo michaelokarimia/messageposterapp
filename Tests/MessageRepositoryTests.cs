e7af7de84becb371be692696e5cbb43f90656e25
Michael Okarimia <git@michaelokarimia.com>
2017-12-01 12:53:53 +0000

    Adds tests for multiple messages of multiple users

--- Tests/MessageRepositoryTests.cs
+++ Tests/MessageRepositoryTests.cs
@@ -51,5 +51,31 @@ namespace Tests
             Assert.AreEqual(message2, messages[1]);
         }
 
+
+        [Test]
+        public void MultipleUsersHaveTheirMessagesStored()
+        {
+            var userNameOne = "David";
+            var message1 = "Ground control to Major Tom";
+            var message2 = "Take your protein pills and put your helmet on";
+
+            var userNameTwo = "Ziggy";
+            var msg1 = "Ziggy played guitar";
+            var msg2 = "jamming good with Weird and Gilly";
+            var msg3 = "And the spiders from Mars;";
+
+            subject.Save(new UserMessage(userNameOne, message1));
+            subject.Save(new UserMessage(userNameOne, message2));
+
+            subject.Save(new UserMessage(userNameTwo, msg1));
+            subject.Save(new UserMessage(userNameTwo, msg2));
+            subject.Save(new UserMessage(userNameTwo, msg3));
+
+            var messages = subject.GetMessages(userNameTwo);
+
+            Assert.AreEqual(msg1, messages[0]);
+            Assert.AreEqual(msg2, messages[1]);
+            Assert.AreEqual(msg3, messages[2]);
+        }
     }
 }
